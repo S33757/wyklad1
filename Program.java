024cefe46cfb844547f3f6cd2f6b074e5d3681af
S33757 <s33757@pjwstk.edu.pl>
2025-03-27 20:26:28 +0100

    zmiana output

--- Program.java
+++ Program.java
@@ -1,9 +1,10 @@
 public class Program
 {
-	public statis void main (String[] args){
-		System.out.println("Hello World!")
+	public static void main (String[] args){
+		System.out.println("Hello World!");
 			int wiek = 10;
 			String imie = "Jacek";
 			String nazwisko = "Kowalski";
+			System.out.println(imie + " " + nazwisko + " lat: " + wiek);
 	}
 }
